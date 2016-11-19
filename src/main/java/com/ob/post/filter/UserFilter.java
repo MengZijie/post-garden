3a09fd323ab3ca824ed516c5f4641b2f9fc01d2d
mengzijie <orafbjaaland@163.com>
2016-11-19 22:53:40 +0800

    ADD: 创建项目

---
+++ src/main/java/com/ob/post/filter/UserFilter.java
@@ -0,0 +1,25 @@
+package com.ob.post.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by OB on 2016/11/19.
+ */
+public class UserFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
+
